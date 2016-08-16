0c83b892a8c74cbcec9bff1e6f4a906bdd3ad0c8
Todd Qualls <todd.qualls@lexisnexis.com>
2016-08-16 11:31:33 -0400

    Adds survey submission results templte.

--- routes/survey.js
+++ routes/survey.js
@@ -12,15 +12,9 @@ router.get('/', function(req, res) {
 router.post('/', function(req, res){
 	//todo: issue #4
 	sheetService.getSpreadsheet(viewName, docName, function(sheet){
-			// req.body.scoutName = 'Sam Smith';
-			// req.body.registeredEmail = 'tqualls@gmail.com';
-			// req.body.additionalEmails = 'troop212bot@gmail.com';
 			sheetService.writeSurvey(sheet, req.body, function(){
 				//todo: issue #11
-				res.render('signup-success', {result: 'This is the wrong result page, but the survey is working!'});
-				console.log('Final callback of writeSurvey function is firing here.');
-				console.log('The target sheet is: ' + sheet.title);
-				//res.render('Survey data written to spreadsheet.');
+				res.render('tech-survey-success', {result: 'Your survey submission has been recorded.'});
 			});
 
 	});
