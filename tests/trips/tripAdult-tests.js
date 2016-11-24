b2c30246cb4b2f93fef0582e450e0eca8f959fdb
Todd E. Qualls <tqualls@gmail.com>
2016-11-24 12:06:04 -0500

    Completes TripAdult type.

--- tests/trips/tripAdult-tests.js
+++ tests/trips/tripAdult-tests.js
@@ -5,11 +5,29 @@ var TripAdult = require('../../types/TripAdult');
 
 describe('TripAdult()', () => {
     it('TripAdult constructor should set all props correctly.', () => {
-        let adult1 = new TripAdult(
+        let adult = new TripAdult(
             'Todd',
             true,
             3,
-            'Ford F-150'
-        );  
+            'Ford F-150',
+            true,
+            false,
+            true,
+            false,
+            true,
+            ['wfa','weather'],
+            ['everything','beer']
+        ); 
+        expect(adult.name).to.equal('Todd');
+        expect(adult.isdriver).to.equal(true);
+        expect(adult.passengercapacity).to.equal(3);
+        expect(adult.carmakemodel).to.equal('Ford F-150');
+        expect(adult.yptcurrent).to.equal(true);
+        expect(adult.healthformscurrent).to.equal(false);
+        expect(adult.haswaiversigned).to.equal(true);
+        expect(adult.isgrubmaster).to.equal(false);
+        expect(adult.feespaid).to.equal(true);
+        expect(adult.certifications).to.deep.equal(['wfa', 'weather']);
+        expect(adult.allergies).to.deep.equal(['everything','beer']);
     });
 });
