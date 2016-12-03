7f17809dfa0e147112aa8cb6d7f32d0ab960e159
Todd E. Qualls <tqualls@gmail.com>
2016-12-02 21:43:36 -0500

    - Adds post to trip route for creating trip. - Starts trip details read only view #43, #50

--- routes/trip.js
+++ routes/trip.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var router = express.Router();
 var viewName = 'trip';
+var Trip = require('../types/Trip.js');
 var scoutingSeason = '2016-2017';
 
 router.get('/', function(req,res){
@@ -10,6 +11,36 @@ router.get('/', function(req,res){
         mode: 'Create a New Trip',
         scoutingSeason: scoutingSeason
      });
+});
+
+router.post('/', function(req, res) {
+        let trip = req.body;
+        let newTrip = new Trip(
+                trip.name,
+                trip.tripmaster,
+                trip.destination,
+                trip.scoutseason,
+                trip.muster_time,
+                trip.departure_time + ' ' + trip.departure_date,
+                trip.return_time + ' ' + trip.return_date,
+                trip.youthfee,
+                trip.adultfee,
+                trip.reqpermissionslip,
+                trip.reqhealthform,
+                trip.reqwaiver,
+                trip.grubmaster,
+                trip.grubfee,
+                [], [], [], [],
+                trip.description
+        );
+        newTrip.create(function() {
+                res.render('trip_details', {
+                        tripId: newTrip.tripid,
+                        name: newTrip.name,
+                        description: newTrip.description
+                });
+        });
+        
 });
 
 module.exports = router;
