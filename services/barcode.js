85213b6e5214c1630477ffa4eb5ff8037be7f7ad
Todd Qualls <Todd Qualls>
2017-03-21 20:42:50 -0400

    Creates barcode.js service.

---
+++ services/barcode.js
@@ -0,0 +1,20 @@
+/*
+    The following is an example of a link to the policy contract page
+
+    http://api-bwipjs.rhcloud.com/?bcid=azteccode&text=http%3A%2F%2Ftroop212-apps.azurewebsites.net%2Fpolicy%2Ftech-contract&format=full
+*/
+'use strict';
+
+let barCodeBaseUrl = 'http://api-bwipjs.rhcloud.com/?bcid=';
+let barCodeType = 'azteccode&text=';
+let troop212TechChipSegment = '=http%3A%2F%2Ftroop212-apps.azurewebsites.net%2Fpolicy%2Ftech-card%2F';
+let finalSegment = '&format=full';
+
+function createBarcodeUrl (contract) {
+    let url = barCodeBaseUrl + barCodeType + troop212TechChipSegment + contract.contractid + finalSegment;
+    return url;
+}
+
+module.exports = {
+    createBarcodeUrl = createBarcodeUrl
+}
