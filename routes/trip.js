1d4ad1fd1be71825e8a35b194e42ebe576669bb1
Todd E. Qualls <tqualls@gmail.com>
2017-06-14 08:36:26 -0400

    Adds iterator to trip list view to show all trips in the current season.

--- routes/trip.js
+++ routes/trip.js
@@ -94,15 +94,9 @@ function getTripById(req, res, viewToRender, mode) {
 router.get('/', function (req, res) {
         let newTrip = new Trip();
         newTrip.getTripsBySeason(scoutingSeason, function (trips) {
-                console.log(trips);
                 res.render(tripListViewName, {
                         scoutSeason: scoutingSeason,
-                        tripId1: 'a4o068j9',
-                        trip1Name: 'THis is the first trip',
-                        tripId2: '3vx7c9y9',
-                        trip2Name: 'This is the 2nd trip name.',
                         trips: trips
-
                 });
         });
 
